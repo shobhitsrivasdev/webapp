3ceab1a8983042e0696b7fc077d3c946b3a5ff59
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-10-02 21:45:17 -0400

    changes

--- src/test/healthz.spec.js
+++ src/test/healthz.spec.js
@@ -8,10 +8,6 @@ dotenv.config()
 const app = express();
 app.use(bodyParser.json());
 app.use(router);
-
-console.log("ENV---", process.env);
-console.log("PORTTT---", process.env.PGDATABASE);
-console.log("ENV---", process.env.NODE_ENV);
 describe("Healthz Endpoint", () => {
   it("should return 200 for successful GET requests without body or query", async () => {
     // Mock successful DB connection for this test
