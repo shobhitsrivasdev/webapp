7894db8908ce75ccc01e58670f9dd1460eb0f71c
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-10-22 22:08:01 -0400

    Adding ssl options

--- src/models/index.js
+++ src/models/index.js
@@ -2,16 +2,29 @@ import Sequelize from "sequelize";
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+const isAwsRDS = process.env.PGHOST && process.env.PGHOST.includes('.rds.amazonaws.com');
+
+const sequelizeConfig = {
+  dialect: 'postgres',
+  host: process.env.PGHOST,
+  operatorsAliases: false,
+  port: process.env.PGPORT,
+};
+ 
+if (isAwsRDS) {
+  sequelizeConfig.dialectOptions = {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  };
+}
+
 export const sequelize = new Sequelize(
   process.env.PGDATABASE,
   process.env.PGUSER,
   process.env.PGPASSWORD,
-  {
-    host: process.env.PGHOST,
-    dialect: "postgres",
-    operatorsAliases: false,
-    port: process.env.PGPORT,
-  }
+  sequelizeConfig
 );
 
 var db = {};
