9b506eaba07dd97846a0838841929a46b648b8fd
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-10-23 21:56:59 -0400

    adding

--- install.sh
+++ install.sh
@@ -1,18 +1,3 @@
-sudo groupadd csye6225
-sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
-
-# Set directory permissions
-sudo find /home/csye6225/web2/ -type d -exec chmod 755 {} \;
-
-# Set file permissions
-sudo find /home/csye6225/web2/ -type f -exec chmod 644 {} \;
-
-# If there are specific executable files, set their permissions
-sudo chmod 755 /home/csye6225/web2/setup.sh
-sudo chmod 755 /home/csye6225/web2/install.sh
-
-sudo chmod 600 /home/csye6225/web2/.env
-
 sudo cp /tmp/nodeserver.service /lib/systemd/system/nodeserver.service
 echo "Enabling the REST API Service"
 sudo systemctl enable nodeserver
--- setup.sh
+++ setup.sh
@@ -20,6 +20,20 @@ sudo apt-get -y remove git
 # Check Node.js version
 nodejs -v
 #sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD  'password'";
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
+
+# Set directory permissions
+sudo find /home/csye6225/web2/ -type d -exec chmod 755 {} \;
+
+# Set file permissions
+sudo find /home/csye6225/web2/ -type f -exec chmod 644 {} \;
+
+# If there are specific executable files, set their permissions
+sudo chmod 755 /home/csye6225/web2/setup.sh
+sudo chmod 755 /home/csye6225/web2/install.sh
+
+sudo chmod 600 /home/csye6225/web2/.env
 
 mkdir web2
 unzip webapp.zip -d web2
