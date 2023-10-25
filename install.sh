2144b5d0e5976c81d12b9e0337d369bae7209cf8
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-10-25 17:54:35 -0400

    adding changes

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-sudo cp /tmp/nodeserver.service /lib/systemd/system/nodeserver.service
+sudo chown -R csye6225:csye6225 /opt/csye6225
+sudo chmod -R 750  /opt/csye6225
+sudo -u csye6225 bash
+
+sudo cp /tmp/nodeserver.service /etc/systemd/system/nodeserver.service
 echo "Enabling the REST API Service"
 sudo systemctl enable nodeserver
 sudo systemctl start nodeserver
--- setup.sh
+++ setup.sh
@@ -4,10 +4,6 @@ sudo cp /tmp/webapp.zip /opt/webapp.zip
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/csye6225 -m csye6225
 
-sudo chown -R csye6225:csye6225 /opt/csye6225
-sudo chmod -R 750  /opt/csye6225
-sudo -u csye6225 bash
-
 sudo apt update
 sudo apt upgrade -y
 sudo apt install -y nodejs
@@ -18,7 +14,7 @@ nodejs -v
 
 cd /opt
 sudo unzip webapp.zip -d csye6225
-cd csye6225
+cd /opt/csye6225
 sudo ls -la
 sudo npm install
  
