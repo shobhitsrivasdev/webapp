7d4fe2d3cbce3712a331767992d75068ada77d21
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-10-24 20:10:42 -0400

    Adding pcaker file

--- setup.sh
+++ setup.sh
@@ -1,50 +1,51 @@
 #!/bin/bash
 
-# Update and upgrade packages
 sudo apt update
 sudo apt upgrade -y
-
-# Install Node.js, npm, and unzip
-sudo apt install -y nodejs npm unzip
-
-# Remove git
+sudo apt install -y nodejs
+sudo apt install -y npm
+sudo apt install -y unzip
 sudo apt-get -y remove git
-
-# Check Node.js version
 nodejs -v
 
-# Create user and group
-sudo groupadd csye6225
-sudo useradd -m -s /bin/bash -g csye6225 csye6225
+# Create a non-admin user
+sudo useradd -m webappuser
+sudo groupadd webappgroup
+# Add webappuser and admin to the webappgroup
+sudo usermod -aG webappgroup webappuser
+sudo usermod -aG webappgroup admin
 
-# Set directory permissions for home
-sudo chown csye6225:csye6225 /home/csye6225
-sudo chmod 770 /home/csye6225
+# switch to non-admin user
+su - webappuser
 
-# Set directory for the web app
-WEB_APP_DIR="/home/csye6225"
+mkdir -p /home/webapp
 
-# Create the directory
-sudo mkdir -p $WEB_APP_DIR
+# Set ownership and permissions for webappuser's home directory
+sudo chown -R webappuser:webappgroup /home/webapp
+sudo chmod -R 750 /home/webapp
+cp /tmp/webapp.zip /home/webapp/webapp.zip
 
-# Unzip the web app into the directory
-sudo unzip /home/csye6225/webapp.zip -d $WEB_APP_DIR
-
-# Set directory permissions
-sudo chown -R csye6225:csye6225 $WEB_APP_DIR
-sudo find $WEB_APP_DIR -type d -exec chmod 770 {} \;
-
-# Set file permissions
-sudo find $WEB_APP_DIR -type f -exec chmod 660 {} \;
-
-# If there are specific executable files, set their permissions
-
-sudo mv /tmp/webapp.zip /home/csye6225/
-
-sudo chmod 755 $WEB_APP_DIR/setup.sh
-sudo chmod 755 $WEB_APP_DIR/install.sh
-sudo chmod 600 $WEB_APP_DIR/.env
-
-# Install npm packages
-cd $WEB_APP_DIR
-sudo npm install
+cd /home/webapp/
+unzip webapp.zip
+ls -la
+npm install
+ 
+# Set ownership and permissions for webappuser's home directory
+sudo chown -R webappuser:webappgroup /home/webappuser
+sudo chmod -R 750 /home/webappuser
+ 
+# Set ownership and permissions for the app.js file in admin's directory
+sudo chown webappuser:webappgroup /home/admin/webapp/app.js
+sudo chmod 750 /home/admin/webapp/app.js
+ 
+# Add webappuser to the systemd-journal group
+sudo usermod -aG systemd-journal webappuser
+ 
+# Set the .env file in admin's directory
+sudo chmod 644 /home/admin/webapp/.env
+ 
+# Create the log file and set ownership and permissions
+sudo touch /var/log/webapp.log
+sudo chown webappuser:webappgroup /var/log/webapp.log
+sudo chmod 644 /var/log/webapp.log
+ 
