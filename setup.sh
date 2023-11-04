ce18bbc51034433e8b17bc0980421ae5bc1094d4
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-11-03 22:20:08 -0400

    Adding git changes

--- setup.sh
+++ setup.sh
@@ -10,6 +10,20 @@ sudo apt install -y nodejs
 sudo apt install -y npm
 sudo apt install -y unzip
 sudo apt-get -y remove git
+sudo apt-get update -y
+sudo apt-get upgrade -y
+echo 'Downloading the CloudWatch Agent package...'
+sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+ 
+echo 'Installing the CloudWatch Agent package...'
+sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+ 
+echo 'Enabling the CloudWatch Agent service...'
+sudo systemctl enable amazon-cloudwatch-agent
+sudo systemctl start amazon-cloudwatch-agent
+ 
+rm ./amazon-cloudwatch-agent.deb
+
 nodejs -v
 
 cd /opt
