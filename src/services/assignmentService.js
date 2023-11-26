b3571ac61726fa75c355e6a8895d0bf81c3b1968
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-11-26 12:27:25 -0500

    Adding region for sns

--- src/services/assignmentService.js
+++ src/services/assignmentService.js
@@ -5,6 +5,10 @@ import logger from "../../configs/logger.config.js";
 import AWS from "aws-sdk";
 import * as dotenv from "dotenv";
 dotenv.config();
+
+AWS.config.update({
+  region: process.env.AWS_REGION
+});
 const sns = new AWS.SNS();
 
 export const create = async (request, response) => {
