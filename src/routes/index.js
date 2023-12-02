2619056e3d76470a257c6a0cab0ce7239ae7f9e9
Shobhit Srivastava <113073011+shobhitsrivastavaneu@users.noreply.github.com>
2023-12-02 17:02:28 -0500

    Adding refresh changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ import assignmentRouter from "./assignments.route.js";
 
 export default (app) => {
   app.use(healthzRouter);
-  app.use("/v1/", assignmentRouter);
+  app.use("/v2/", assignmentRouter);
   
 };
